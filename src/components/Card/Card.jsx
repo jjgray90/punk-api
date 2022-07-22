065416adf203e4680c62dc3839c29c1fcb28dfa2
Your Name <jjgray90@gmail.com>
2022-07-22 14:05:34 +0100

    hover styles

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,7 +4,11 @@ const Card = ({ beer }) => {
   return (
     <section className="card">
       <img src={beer.image_url} alt={beer.name} className="card__image" />
-      <h2 className="card__heading">{beer.name}</h2>
+      <div className="card__hidden">
+        <h2 className="hidden__heading">{beer.name}</h2>
+        <p>{beer.abv}% ABV</p>
+        <p>{beer.tagline}</p>
+      </div>
     </section>
   );
 };
